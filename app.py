062b6797c91fb093e52c278044597d3530e30088
alexandershearer <alex.shearer23@gmail.com>
2020-09-30 18:55:51 -0500

    Added the first function reading the poem

---
+++ app.py
@@ -0,0 +1,52 @@
+import random
+
+#Opem the txt file and store it into a variable
+poem = open('./poem.txt', 'r')
+
+#Function to print every line of the txt file
+def get_file_lines(filename):   
+
+    for line in filename:
+        print(line)
+
+get_file_lines(poem)
+print("--------------------------")
+print("--------------------------")
+
+#Function to print the entire poem starting with last line
+def lines_printed_backwards(filename):
+    #Store each line from the poem into a list
+    line_list = [line.strip() for line in filename]
+        #Reverse the list and print each line in the reversed order
+    for line in reversed(line_list):
+        print(line)
+
+#lines_printed_backwards(poem)
+print("--------------------------")
+print("--------------------------")
+
+#Function to randomly print a line from the poem for the length of the poem
+def lines_printed_random(filename):
+
+    line_list = [line.strip() for line in filename]
+    #Get the length of the list minus 1 since the first object is 0
+    length = len(line_list) - 1
+    #Make a loop that only runs the length of the poem and gets a random num from 0 to the length and print 
+    # the line that matches that list id
+    for _ in range(0, length):
+        random_num = random.randint(0, length)
+        print(line_list[random_num])
+
+    
+#lines_printed_random(poem)   
+print("--------------------------")
+print("--------------------------")
+
+def lines_printed_custom(filename):
+    line_list = [line.strip() for line in filename]
+
+    for line in line_list:
+        #Print every line in all uppercase 
+        print(line.upper())
+
+#lines_printed_custom(poem)
